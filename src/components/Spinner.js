21110cc7e55aaf3602bd9f77b3fa035418ed8aeb
Mahaveer sharma <Mahaveer sharma>
2017-04-05 10:15:21 +0530

    added spinner/loader :D

---
+++ src/components/Spinner.js
@@ -0,0 +1,7 @@
+import React, { Component } from 'react';
+
+export default function Spinner(props) {
+   return(
+      <div className="spinner"></div>
+   )
+}
--- src/index.js
+++ src/index.js
@@ -6,7 +6,8 @@ import * as SC from 'soundcloud';
 // COMPONENTS-------------------
 import Nav_bar_container from './components/Nav_bar_container';
 import List_items from './components/List_items';
-import Player from './components/Player'
+import Player from './components/Player';
+import Spinner from './components/Spinner'
 
 
 class App extends Component {
@@ -25,17 +26,20 @@ class App extends Component {
          urlOfNextTrack: '',
          urlOfPreviousTrack: '',
          showPlayer: false,
+         showListItems: false,
+         showSpinner: true,
          qParam: 'original'
       }
    }
    render() {
-      let { tracks, streamUrl, title, genre, songImg, indexOfTrack, urlOfNextTrack, urlOfPreviousTrack, showPlayer } = this.state
+      let { tracks, streamUrl, title, genre, songImg, indexOfTrack, urlOfNextTrack, urlOfPreviousTrack, showListItems, showPlayer, showSpinner } = this.state
       return (
          <div ref="appContainer" className="app-container">
             <Nav_bar_container qParamHandler={this.qParamHandler} searchHandler={this.searchHandler} />
-            <List_items
+            {showSpinner && <Spinner />}
+            {showListItems && <List_items
                tracks={tracks}
-               onClickHandler={this.clickHandler} />
+               onClickHandler={this.clickHandler} />}
             {showPlayer &&
                <Player
                   streamUrl={streamUrl}
@@ -49,30 +53,32 @@ class App extends Component {
       )
    }
    qParamHandler(qParam) {
-      let appContainer = this.refs.appContainer
-      appContainer.style.cursor = 'wait';
+      this.setState({
+         showListItems: false,
+         showSpinner: true
+      })
       SC.get('/tracks', {
          q: qParam, limit: 10
       }).then(data => {
          this.setState({
             tracks: data,
-         }, () => {
-            console.log(`state is set cursor not changed wtf`);
-            appContainer.style.cursor = 'default';
+            showListItems: true,
+            showSpinner: false
          })
       });
    }
    searchHandler(searchTerm) {
-      let appContainer = this.refs.appContainer
-      appContainer.style.cursor = 'wait';
+      this.setState({
+         showListItems: false,
+         showSpinner: true
+      })
       SC.get('/tracks', {
          q: searchTerm, limit: 10
       }).then(data => {
          this.setState({
             tracks: data,
-         }, () => {
-            console.log(`state is set cursor not changed wtf`);
-            appContainer.style.cursor = 'default';
+            showSpinner: false,
+            showListItems: true
          })
       });
    }
@@ -90,9 +96,6 @@ class App extends Component {
       })
    }
    componentDidMount() {
-      let appContainer = this.refs.appContainer
-      appContainer.style.cursor = 'wait';
-
       SC.initialize({
          client_id: '340f063c670272fac27cfa67bffcafc4'
       });
@@ -100,14 +103,12 @@ class App extends Component {
          q: this.state.qParam, limit: 20, offset: 0
       }).then(data => {
          this.setState({
-            tracks: data
-         }, () => {
-            console.log(`state is set cursor not changed wtf`);
-            appContainer.style.cursor = 'default';
-
+            tracks: data,
+            showListItems: true,
+            showSpinner: false
          })
       });
-   }
+   }   
 }
 
 ReactDOM.render(<App />, document.querySelector('.app'));
