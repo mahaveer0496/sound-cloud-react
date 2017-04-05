eaacbaf1233f186273c9bc521bae0fceca81f4c7
Mahaveer sharma <Mahaveer sharma>
2017-04-06 00:22:21 +0530

    spinner style updated

--- src/components/Lower_nav.js
+++ src/components/Lower_nav.js
@@ -3,14 +3,20 @@ import React, { Component } from 'react'
 export default class Lower_nav extends Component {
    constructor(props) {
       super(props);
-      this.clickHandler = this.clickHandler.bind(this)
+      this.clickHandler = this.clickHandler.bind(this);
+      this.state = {
+         li_className : ''
+      }
    }
    clickHandler(e) {
       console.log(e.target.innerHTML)
       this.props.qParamHandler(e.target.innerHTML);
+      this.setState({
+         li_className: 'li-active'
+      })
    }
    render() {
-      let listArray = ['Chill', 'Deep', 'Dubstep', 'House', 'Progressive', 'Tech', 'Trance'].map(val => <li key={val}><a onClick={this.clickHandler} href="#">{val}</a></li>)
+      let listArray = ['Chill', 'Deep', 'Dubstep', 'House', 'Progressive', 'Tech', 'Trance'].map(val => <li className={`lower-nav-li ${this.state.li_className}`} onClick={this.clickHandler} key={val}>{val}</li>)
 
       return (
          <nav className="lower-nav">
--- src/components/Spinner.js
+++ src/components/Spinner.js
@@ -1,7 +1,13 @@
 import React, { Component } from 'react';
 
 export default function Spinner(props) {
-   return(
-      <div className="spinner"></div>
+   return (
+      <div className="spinner">
+         <div className="rect1"></div>
+         <div className="rect2"></div>
+         <div className="rect3"></div>
+         <div className="rect4"></div>
+         <div className="rect5"></div>
+      </div>
    )
 }
