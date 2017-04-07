b62ea197ab8c559ef8e3d0160d42eba85fa62aae
Mahaveer sharma <Mahaveer sharma>
2017-04-08 01:34:21 +0530

    List_item UI enhancement - phase 1

--- src/components/List_item.js
+++ src/components/List_item.js
@@ -4,8 +4,12 @@ export default class List_item extends Component {
    constructor(props) {
       super(props);
       this.handleClick = this.handleClick.bind(this);
+      this.state = {
+         currentTrackPlayState: false
+      }
    }
-   handleClick() {
+   handleClick(e) {
+      console.log(e);
       console.log(`handleClick was called - ${this.props.streamUrl}`);
       let { songImg, streamUrl, urlOfNextTrack, urlOfPreviousTrack, trackUrlHandler, onClickHandler, title, genre, indexOfTrack } = this.props;
       let showPlayer = true;
@@ -19,31 +23,39 @@ export default class List_item extends Component {
          urlOfPreviousTrack,
          showPlayer
       }
-      trackUrlHandler(indexOfTrack); //from List_item.js
-      onClickHandler(trackInfoObj)
+      this.setState({
+         currentTrackPlayState: true
+      })
+   
+   trackUrlHandler(indexOfTrack); //from List_item.js
+   onClickHandler(trackInfoObj)
 
-   }
-   render() {
-      let { songImg, userImg, title, genre } = this.props;
-      return (
+}
+render() {
+   let { songImg, userImg, title, genre } = this.props;
+   return (
 
-         <div onClick={this.handleClick} className="list-item active">
-            <div className="song-image" style={{
-               backgroundImage: `url(${songImg})`,
-               width: `${100}%`,
-               height: `${70}px`,
-               backgroundRepeat: 'no-repeat',
-               backgroundSize: 'cover',
-               backgroundPosition: `${50}%`
+      <div onClick={this.handleClick} className="list-item active">
+         <div className="song-image" style={{
+            backgroundImage: `url(${songImg})`,
+            width: `${100}%`,
+            height: `${70}px`,
+            backgroundRepeat: 'no-repeat',
+            backgroundSize: 'cover',
+            backgroundPosition: `${50}%`
 
-            }}></div>
-            <div className="song-details">
-               <img src={userImg} alt="user-avatar" className="user-image" />
-               <a href="#">{title}</a>
-               <a href="#" className="user-name">{genre}</a>
-            </div>
-         </div >
+         }}>
+            {this.state.currentTrackPlayState && <div className="track-active">
+               <div className="ion-play"></div>
+            </div>}
+         </div>
+         <div className="song-details">
+            <img src={userImg} alt="user-avatar" className="user-image" />
+            <a href="#">{title}</a>
+            <a href="#" className="user-name">{genre}</a>
+         </div>
+      </div >
 
-      )
-   }
+   )
+}
 }
