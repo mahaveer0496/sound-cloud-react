032522fd03c87f9e99acf90340c094cccb633d1d
Mahaveer <mahaveersharma0496@yahoo.com>
2017-04-21 20:50:25 +0530

    no comment

--- server.js
+++ server.js
@@ -3,13 +3,13 @@ const express = require('express');
 // create our app
 var app = express();
 const PORT = process.env.PORT || 3000;
-app.use(function (req, res, next) {
-  if (req.headers['x-forwarded-proto'] === 'https') {
-    res.redirect('http://' + req.hostname + req.url)
-  } else {
-    next();
-  }
-});
+// app.use(function (req, res, next) {
+//   if (req.headers['x-forwarded-proto'] === 'https') {
+//     res.redirect('http://' + req.hostname + req.url)
+//   } else {
+//     next();
+//   }
+// });
 app.use(express.static('public'));
 
 app.listen(PORT, function () {
