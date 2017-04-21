11b528e27e9e9012f104b4535e77d7b9fb5f0952
Mahaveer <mahaveersharma0496@yahoo.com>
2017-04-21 21:15:50 +0530

    lalala

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ var app = express();
 const PORT = process.env.PORT || 3000;
 app.use(function (req, res, next) {
   if (req.headers['x-forwarded-proto'] === 'https') {
-    res.redirect('https://' + req.hostname + req.url)
+    res.redirect('http://' + req.hostname + req.url)
   } else {
     next();
   }
